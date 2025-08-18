c6349a8e0161c9c5e148d519289563d9958fc65e
int 21jh <145056912+Gchipunov@users.noreply.github.com>
2025-08-17 21:14:53 -0700

    Create view-size.js

---
+++ PropperWebGLCanvas/view-size.js
@@ -0,0 +1,18 @@
+
+
+const viewportWidth = window.innerWidth;
+const viewportHeight = window.innerHeight;
+
+console.log(`The viewport is ${viewportWidth}px wide and ${viewportHeight}px high.`);
+
+
+const screenWidth = screen.width;
+const screenHeight = screen.height;
+
+console.log(`The user's screen resolution is ${screenWidth}x${screenHeight}.`);
+
+const outerWidth = window.outerWidth;
+const outerHeight = window.outerHeight;
+
+console.log(`The browser window is ${outerWidth}px wide and ${outerHeight}px high.`);
+
